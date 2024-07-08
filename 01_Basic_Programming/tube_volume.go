6e7b766910ee3f3eba041ffb2c076d75ae06cd7f
hamid4231 <mrjby8596@gmail.com>
2024-07-08 09:40:07 +0700

    new code

--- 01_Basic_Programming/tube_volume.go
+++ 01_Basic_Programming/tube_volume.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var radius int
+	var height int
+	var volume float64
+	pi := 3.14
+	fmt.Print("Enter The Radius: ")
+	fmt.Scan(&radius)
+	fmt.Print("Enter The Height: ")
+	fmt.Scan(&height)
+	volume = pi * float64(radius*radius) * float64(height)
+	fmt.Println("The volume of your tube is: ", volume)
 }
