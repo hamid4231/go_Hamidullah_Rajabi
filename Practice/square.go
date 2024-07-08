901c7e0f2e577918b13e39701bda2944bba030bf
hamid4231 <mrjby8596@gmail.com>
2024-07-08 20:28:31 +0700

    new practice

--- Practice/square.go
+++ Practice/square.go
@@ -7,7 +7,7 @@ func main() {
 	fmt.Print("How many sequence of number do you want to have the squared? ")
 	fmt.Scan(&number)
 
-	for i:= 1, i>= number, i++{
-		fmt.Println(i*i)
+	for i := 1; i <= number; i++ {
+		fmt.Println(i * i)
 	}
 }
